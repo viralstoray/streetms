6ce5288bee62a062545622efbf88c209742ff5b1
sheppardzwc@gmail.com <sheppardzwc@gmail.com@3243108e-d613-f714-1ed2-81b2b74c7241>
2011-08-12 22:02:09 +0000

    no need for this

--- SQL/lottery.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE  `lottery` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `amount` int(10) unsigned NOT NULL DEFAULT '0',
-  `winner` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;

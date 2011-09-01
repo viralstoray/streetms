ec9fbb35fcf46df6b9cd40dc0b65f8ff76708dbc
Flame1Rock@gmail.com <Flame1Rock@gmail.com@3243108e-d613-f714-1ed2-81b2b74c7241>
2011-09-01 01:16:41 +0000

    Aran is now fully coded! Fixed a few GM commands/added a few.

---
+++ scripts/quest/21400.js
@@ -0,0 +1,45 @@
+/*
+	This file is part of the OdinMS Maple Story Server
+    Copyright (C) 2008 Patrick Huy <patrick.huy@frz.cc>
+		       Matthias Butz <matze@odinms.de>
+		       Jan Christian Meyer <vimes@odinms.de>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation version 3 as published by
+    the Free Software Foundation. You may not use, modify or distribute
+    this program under any other version of the GNU Affero General Public
+    License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*	
+	Author : Biscuit
+*/
+var status = -1;
+
+function start(mode, type, selection) {
+    status++;
+    if (mode != 1) {
+		if(type == 1 && mode == 0) {
+			qm.dispose();
+			return;
+		}else{
+			qm.dispose();
+			return;
+		}
+	}
+	
+	if (status == 0) {
+		qm.sendAcceptDecline("How is the training going? I know you're busy, but please come to #bRien#k immediately. #bMaha#k has started to act weird again... But it's even weirder now. It's different from before. It's...darker than usual.");
+	} else if (status == 1) {
+		qm.forceStartQuest();
+		qm.dispose();
+	}
+}
